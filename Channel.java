spring/SpringLS

commit 566760418dc73849a3404256e2d8079cf61f9fd4

* moved client logic to Clients.java, battles to Battles.java, channels to Channels.java * improved OO structure of the source * now also keeping local list of channels user is participating in (Client.channels) * battle participants may now use /ring on their host * enforced restrictions on UPDATEBATTLEDETAILS command * SAY and SAYEX commands are now ignored if user is not actually participating in the channel * fixed bug with statistics not taking in account battles with only 2 players
git-svn-id: http://springrts.com/svn/spring/trunk/TASServer@2557 37977431-3df6-0310-b722-df95706aa16b

Channel.java
@@ -20,14 +20,18 @@ public class Channel {
 	private String topicAuthor;
 	private long topicChangedTime; // time when topic was last changed (in ms since Jan 1, 1970 UTC)
 	private String key = ""; // if key is "" then this channel is not locked (anyone can join). Otherwise, user must supply correct key to join it. 
-	public ArrayList clients; // clients connected to this channel
+	private ArrayList<Client> clients; // clients participating in this channel
 	public MuteList muteList = new MuteList(this); // contains a list of Strings (usernames) who are muted (not allowed to talk in the channel)
 	
 	public Channel(String channelName) {
 		name = new String(channelName);
 		topic = "";
 		topicAuthor = "";
-		clients = new ArrayList();
+		clients = new ArrayList<Client>();
+	}
+	
+	public boolean equals(Channel chan) {
+		return this.name.equals(chan.name);
 	}
 	
 	public String getTopic() {
@@ -66,7 +70,7 @@ public class Channel {
 		return !(topic.equals(""));
 	}
 	
-	/* adds a new client to the listeners of this channel */
+	/* adds new client to the list of clints of this channel */
 	public void addClient(Client client) {
 		if (isClientInThisChannel(client)) return ; // already in the channel! 
 
@@ -81,13 +85,27 @@ public class Channel {
 		return (clients.indexOf(client) != -1);
 	}
 	
+	/* returns number of clients in this channel */
+	public int getClientsSize() {
+		return clients.size();		
+	}
+	
+	/*  returns null if index if out of bounds */
+	public Client getClient(int index) {
+		try {
+			return clients.get(index);
+		} catch(IndexOutOfBoundsException e) {
+			return null;
+		}
+	}
+	
 	/* sends s to all clients in this channel */
 	public void sendLineToClients(String s) {
 		if (name.toUpperCase().equals("MAIN")) if (TASServer.LOG_MAIN_CHANNEL) {
 			TASServer.writeMainChanLog(s);
 		}
 		for (int i = 0; i < clients.size(); i++)
-			((Client)clients.get(i)).sendLine(s);
+			clients.get(i).sendLine(s);
 	}
 	
 	public boolean isLocked() {

Channels.java
@@ -1,0 +1,85 @@
+/*
+ * Created on 2006.11.2
+ *
+ */
+
+/**
+ * @author Betalord
+ *
+ */
+
+import java.util.ArrayList;
+
+public class Channels {
+	
+	static private ArrayList<Channel> channels = new ArrayList<Channel>();
+	
+	/* returns null if channel does not exist (is not open) */
+	public static Channel getChannel(String chanName) {
+		for (int i = 0; i < channels.size(); i++) {
+			if (channels.get(i).name.equals(chanName)) return channels.get(i);
+		}
+		return null;
+	}
+	
+	/* returns null if index is out of bounds */
+	public static Channel getChannel(int index) {
+		try {
+			return channels.get(index);
+		} catch (IndexOutOfBoundsException e) {
+			return null;
+		}
+	}	
+	
+	public static int getChannelsSize() {
+		return channels.size();
+	}
+	
+	/* removes channel from channel list. Returns true if channel was found. */
+	public static boolean removeChannel(Channel chan) {
+		return channels.remove(chan);
+	}
+	
+	/* sends information on all clients in a channel (and the topic if it is set) to the client */
+	public static boolean sendChannelInfoToClient(Channel chan, Client client) {
+		// it always sends info about at least one client - the one to whom this list must be sent
+		String s = "CLIENTS " + chan.name;
+		int c = 0;
+		
+		for (int i = 0; i < chan.getClientsSize(); i++) {
+			s = s.concat(" " + chan.getClient(i).account.user);
+			c++;
+			if (c > 10) { // 10 is the maximum number of users in a single line
+				client.sendLine(s);
+				s = "CLIENTS " + chan.name;
+				c = 0;
+			}
+		}
+		if (c > 0) {
+			client.sendLine(s);
+		}
+			
+		// send the topic:
+		if (chan.isTopicSet()) client.sendLine("CHANNELTOPIC " + chan.name + " " + chan.getTopicAuthor() + " " + chan.getTopicChangedTime() + " " + chan.getTopic());
+		
+		return true;
+	}
+	
+	/* sends a list of all open channels to client */
+	public static void sendChannelListToClient(Client client) {
+		if (channels.size() == 0) return ; // nothing to send
+		
+		for (int i = 0; i < channels.size(); i++) {
+			client.sendLine("CHANNEL " + channels.get(i).name + " " + channels.get(i).getClientsSize() + (channels.get(i).isTopicSet() ? " " + channels.get(i).getTopic() : ""));
+		}
+		client.sendLine("ENDOFCHANNELS");
+	}
+	
+	public static void notifyClientsOfNewClientInChannel(Channel chan, Client client) {
+		for (int i = 0; i < chan.getClientsSize(); i++) {
+			if (chan.getClient(i) == client) continue;
+			chan.getClient(i).sendLine("JOINED " + chan.name + " " + client.account.user);
+		}
+	}
+
+}

Client.java
@@ -14,7 +14,9 @@
 
 //import java.io.*;
 //import java.net.*;
+
 import java.nio.channels.*;
+import java.util.*;
 
 public class Client {
 	public boolean alive = false;
@@ -27,6 +29,7 @@ public class Client {
 	public int battleStatus; // see MYBATTLESTATUS command for actual values of battleStatus
 	public int teamColor; // see MYBATTLESTATUS for info on this one
 	public int battleID; // battle ID in which client is participating. Must be -1 if not participating in any battle.
+	public ArrayList<Channel> channels = new ArrayList<Channel>(); // list of channels user is participating in
 	
 	public SocketChannel sockChan;
 	public SelectionKey selKey;
@@ -78,23 +81,23 @@ public class Client {
 		try {
 			if (!TASServer.sendLineToSocketChannel(text, sockChan)) {
 				System.out.println("Error writing to socket. Line not sent! Killing the client next loop...");
-				TASServer.killList.add(this);
+				Clients.killClientDelayed(this, "Quit: undefined connection error");
 				return false;
 			}
 		} catch (ChannelWriteTimeoutException e) {
     		System.out.println("WARNING: channelWrite() timed out. Disconnecting client ...");
-			TASServer.sendToAllAdministrators("SERVERMSG [broadcast to all admins]: Serious problem: channelWrite() timed out [" + IP + ", <" + account.user + ">]");
+			Clients.sendToAllAdministrators("SERVERMSG [broadcast to all admins]: Serious problem: channelWrite() timed out [" + IP + ", <" + account.user + ">]");
 			
 			// add server notification:
 			ServerNotification sn = new ServerNotification("channelWrite() timeout");
 			sn.addLine("Serious problem detected: channelWrite() has timed out.");
 			sn.addLine("Client: " + IP + "(" + (account.user.equals("") ? "user not logged in" : account.user) + ")");
 			ServerNotifications.addNotification(sn);
-			TASServer.killClient(this, "channelWrite() timed out");
+			Clients.killClientDelayed(this, "Quit: channelWrite() timed out");
 			
 		} catch (Exception e) {
 			System.out.println("Error writing to socket. Line not sent! Killing the client next loop...");
-			TASServer.killList.add(this);
+			Clients.killClientDelayed(this, "Quit: undefined connection error");
 			return false;
 		}
 		return true;
@@ -104,7 +107,7 @@ public class Client {
 		sendLine("TASServer " + TASServer.VERSION + " " + TASServer.NAT_TRAVERSAL_PORT);
 	}
 	
-	/* should only be called by TASServer.killClient() method! */
+	/* should only be called by Clients.killClient() method! */
 	public void disconnect() {
 		if (!alive) {
 			System.out.println("PROBLEM DETECTED: disconnecting dead client. Skipping ...");
@@ -120,4 +123,58 @@ public class Client {
 		sockChan = null;
 		selKey = null;
 	}
+	
+	/* joins client to <chanName> channel. If channel with that name
+	 * does not exist, it is created. Method returns channel object as a result. 
+	 * If client is already in the channel, it returns null as a result. 
+	 * This method does not check for a correct key in case the channel is locked, 
+	 * caller of this method should do that before calling it.
+	 * This method also doesn't do any notificating of other clients in the channel,
+	 * caller must do all that. */
+	public Channel joinChannel(String chanName) {
+		Channel chan = Channels.getChannel(chanName);
+		if (chan == null) {
+			chan = new Channel(chanName);
+			channels.add(chan);
+		}
+		else if (this.channels.indexOf(chan) == -1) return null; // already in the channel 
+
+		chan.addClient(this);
+		this.channels.add(chan);
+		return chan;
+	}
+	
+	/* removes client from the channel and notifies all other clients in the channel about it.
+	 * If this was the last client in the channel, then channel is removed from channels list. 
+	 * "reason" may be left blank ("") if no reason is to be given. */
+	public boolean leaveChannel(Channel chan, String reason) {
+		boolean result = chan.removeClient(this);
+		
+		if (result) {
+			if (chan.getClientsSize() == 0) Channels.removeChannel(chan); // since channel is empty there is no point in keeping it in a channels list. If we would keep it, the channels list would grow larger and larger in time. We don't want that!
+			else for (int i = 0; i < chan.getClientsSize(); i++) chan.getClient(i).sendLine("LEFT " + chan.name + " " + this.account.user + (reason.equals("") ? "" : " " + reason));
+			this.channels.remove(chan);
+		} 
+		
+		return result;
+	}
+	
+	/* calls leaveChannel() for every channel client is participating in.
+	 * Also notifies all clients of his departure.
+	 * Also see comments for leaveChannel() method. */
+	public void leaveAllChannels(String reason) {
+		for (int i = 0; i < this.channels.size(); i++) leaveChannel(channels.get(i), reason);
+		this.channels.clear();
+	}
+	
+	/* will search the list of channels this user is participating in
+	 * and return the specified channel or null if client is not participating
+	 * in this channel. */
+	public Channel getChannel(String chanName) {
+		for (int i = 0; i < channels.size(); i++) {
+			if (channels.get(i).name.equals(chanName)) return channels.get(i);
+		}
+		return null;		
+	}
+	
 }

Accounts.java
@@ -9,11 +9,6 @@
 /**
  * @author Betalord
  *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
- * 
- * TODO Move writeAccountsInfo() to a separate thread so it won't block when processing
- *      commands like LOGIN, REGISTER, etc. 
  */
 
 import java.io.BufferedReader;
@@ -155,8 +150,13 @@ public class Accounts {
 		return map.get(username);
 	}
 
+	/* returns null if index is out of bounds */
 	public static Account getAccount(int index) {
-		return accounts.get(index);
+		try {
+			return accounts.get(index);
+		} catch (IndexOutOfBoundsException e) {
+			return null;
+		}
 	}
 	
 	public static Account findAccountNoCase(String username) {

BanList.java
@@ -21,12 +21,12 @@ import java.util.ArrayList;
  */
 
 public class BanList {
-	private ArrayList ips;
-	private ArrayList reason;
+	private ArrayList<String> ips; // banned IP addresses
+	private ArrayList<String> reason; // reasons for bans (parallel to 'ips' list)
 	
 	public BanList() {
-		ips = new ArrayList();
-		reason = new ArrayList();
+		ips = new ArrayList<String>();
+		reason = new ArrayList<String>();
 	}
 	
 	public void clear() {
@@ -35,8 +35,8 @@ public class BanList {
 	}
 	
 	public void add(String IP, String reason) {
-		this.ips.add(new String(IP));
-		this.reason.add(new String(reason));
+		this.ips.add(IP);
+		this.reason.add(reason);
 	}
 	
 	public boolean remove(String IP) {
@@ -125,6 +125,5 @@ public class BanList {
 		}
 		return -1;
 	}
-
 	
 }

Battle.java
@@ -21,8 +21,8 @@ public class Battle {
 	public int natType; // NAT traversal technique used by the host. Use 0 for none.
 	public String title; // description of the battle set by founder of the battle
 	public Client founder; // founder (host) of the battle
-	public ArrayList clients; // clients without the founder
-	public ArrayList bots;
+	private ArrayList<Client> clients; // clients without the founder (host)
+	private ArrayList<Bot> bots; // bots added by clients participating in this battle
 	public String mapName;
 	public int maxPlayers;
 	public String password; // use restricted() method to find out if battle is password-protected
@@ -35,81 +35,25 @@ public class Battle {
 	public int hashCode; // see notes for description!
 	public int rank; // if 0, no rank limit is set. If 1 or higher, only players with this rank (or higher) can join the battle (Note: rank index 1 means seconds rank, not the first one, since you can't limit game to players of the first rank because that means game is open to all players and you don't have to limit it in that case)
 	public String modName;
-	public ArrayList disabledUnits;
+	public ArrayList<String> disabledUnits;
 	public StartRect[] startRects;
 	public boolean limitDGun;
 	public boolean diminishingMMs;
 	public boolean ghostedBuildings; 
 	public boolean locked; // if true, battle is locked and noone can join it (until lock is released by founder)
 	// following elements are used only with type=1:
-	public ArrayList replayScript = new ArrayList(); // contains lines of the script file
-	public ArrayList tempReplayScript = new ArrayList(); // here we save script lines until we receive SCRIPTEND command. Then we copy it to "replayScript" object and notify all clients about it. 
+	public ArrayList<String> replayScript = new ArrayList<String>(); // contains lines of the script file
+	public ArrayList<String> tempReplayScript = new ArrayList<String>(); // here we save script lines until we receive SCRIPTEND command. Then we copy it to "replayScript" object and notify all clients about it. 
 	
-	
-	
-	/* Creates new Battle object from a command that client sent to server. This method
-	 * parses the command (String s) and tries to read battle attributes from it. If
-	 * unsuccessful, it returns null as a result. */
-	public static Battle createBattleFromString(String s, Client founder) {
-		String[] parsed = s.split(" ");
-		if (parsed.length < 17) return null;
-		String[] parsed2 = Misc.makeSentence(parsed, 16).split("\t");
-		if (parsed2.length != 3) return null;
-		
-		String pass = parsed[3];
-		if (!pass.equals("*")) if (!Misc.isValidName(pass)) return null;
-		
-		int type;
-		int natType;
-		int port;
-		int maxPlayers;
-		int startMetal;
-		int startEnergy;
-		int maxUnits;
-		int startPos;
-		int gameEndCondition;
-		boolean limitDGun;
-		boolean diminishingMMs;
-		boolean ghostedBuildings;
-		int hash;
-		int rank;
-		
-		try {
-			type = Integer.parseInt(parsed[1]);
-			natType = Integer.parseInt(parsed[2]);
-			// parsed[3] is password
-			port = Integer.parseInt(parsed[4]);
-			maxPlayers = Integer.parseInt(parsed[5]);
-			startMetal = Integer.parseInt(parsed[6]);
-			startEnergy = Integer.parseInt(parsed[7]);
-			maxUnits = Integer.parseInt(parsed[8]);
-			startPos = Integer.parseInt(parsed[9]);
-			gameEndCondition = Integer.parseInt(parsed[10]);
-			limitDGun = Misc.strToBool(parsed[11]);
-			diminishingMMs = Misc.strToBool(parsed[12]);
-			ghostedBuildings = Misc.strToBool(parsed[13]);			
-			hash = Integer.parseInt(parsed[14]);
-			rank = Integer.parseInt(parsed[15]);
-		} catch (NumberFormatException e) {
-			return null; 
-		}
-		
-		if ((startPos < 0) || (startPos > 2)) return null;
-		if ((gameEndCondition < 0) || (gameEndCondition > 1)) return null;
-		if ((type < 0) || (type > 1)) return null;
-		if ((natType < 0) || (natType > 2)) return null;
 
-		return new Battle(type, natType, founder, pass, port, maxPlayers, startMetal, startEnergy, maxUnits, startPos, gameEndCondition, limitDGun, diminishingMMs, ghostedBuildings, hash, rank, parsed2[0], parsed2[1], parsed2[2]);
-	}
-	
 	public Battle(int type, int natType, Client founder, String password, int port, int maxPlayers, int startMetal, int startEnergy, int maxUnits, int startPos, int gameEndCondition, boolean limitDGun, boolean diminishingMMs, boolean ghostedBuildings, int hashCode, int rank, String mapName, String title, String modName) {
 		this.ID = IDCounter++;
 		this.type = type;
 		this.natType = natType;
 		this.title = new String(title);
 		this.founder = founder;
-		this.clients = new ArrayList();
-		this.bots = new ArrayList();
+		this.clients = new ArrayList<Client>();
+		this.bots = new ArrayList<Bot>();
 		this.mapName = new String(mapName);
 		this.maxPlayers = maxPlayers;
 		this.password = new String(password);
@@ -125,7 +69,7 @@ public class Battle {
 		this.hashCode = hashCode;
 		this.rank = rank;
 		this.modName = new String(modName);
-		this.disabledUnits = new ArrayList();
+		this.disabledUnits = new ArrayList<String>();
 		this.startRects = new StartRect[10];
 		this.locked = false; // we assume this by default. Client must make sure it is unlocked.
 		for (int i = 0; i < startRects.length; i++) startRects[i] = new StartRect();
@@ -148,8 +92,8 @@ public class Battle {
 	 * of all clients in this battle EXCEPT for himself! */
 	public void notifyOfBattleStatuses(Client client) {
 		for (int i = 0; i < this.clients.size(); i++) 
-			if ((Client)this.clients.get(i) == client) continue;
-			else client.sendLine("CLIENTBATTLESTATUS " + ((Client)this.clients.get(i)).account.user + " " + ((Client)this.clients.get(i)).battleStatus + " " + ((Client)this.clients.get(i)).teamColor);
+			if (this.clients.get(i) == client) continue;
+			else client.sendLine("CLIENTBATTLESTATUS " + this.clients.get(i).account.user + " " + this.clients.get(i).battleStatus + " " + this.clients.get(i).teamColor);
 		if (founder != client) client.sendLine("CLIENTBATTLESTATUS " + founder.account.user + " " + founder.battleStatus + " " + founder.teamColor);
 	}
 	
@@ -162,7 +106,7 @@ public class Battle {
 	/* sends String s to all clients participating in this battle */
 	public void sendToAllClients(String s) {
 		for (int i = 0; i < this.clients.size(); i++) {
-			((Client)clients.get(i)).sendLine(s);
+			clients.get(i).sendLine(s);
 		}
 		founder.sendLine(s);
 	}
@@ -170,16 +114,29 @@ public class Battle {
 	/* sends String s to all clients participating in this battle except for the founder */
 	public void sendToAllExceptFounder(String s) {
 		for (int i = 0; i < this.clients.size(); i++) {
-			((Client)clients.get(i)).sendLine(s);
+			clients.get(i).sendLine(s);
 		}
 	}
 	
-	private String clientsToString() {
+	public String clientsToString() {
 		if (this.clients.size() == 0) return "";
-		String s = new String(((Client)clients.get(0)).account.user);
+		String s = new String(clients.get(0).account.user);
 		for (int i = 1; i < this.clients.size(); i++)
-			s.concat(" " + ((Client)clients.get(i)).account.user);
+			s.concat(" " + clients.get(i).account.user);
 		return s;
+	}
+	
+	/* returns number of clients participating in this battle */
+	public int getClientsSize() {
+		return clients.size();
+	}
+	
+	public Client getClient(int index) {
+		try {
+			return clients.get(index);
+		} catch (IndexOutOfBoundsException e) {
+			return null;
+		}
 	}
 	
 	public boolean addClient(Client client) {
@@ -198,11 +155,13 @@ public class Battle {
 		return Misc.getInGameFromStatus(founder.status) == 1; 
 	}
 	
-	// returns number of spectators in the battle
+	/* returns number of spectators in this battle. Note that this operation is 
+	 * not very fast - we have to go through the entire list of clients in this 
+	 * battle to figure out spectator count. */
 	public int spectatorCount() {
 		int count = 0;
 		for (int i = 0; i < clients.size(); i++)
-			if (Misc.getModeFromBattleStatus(((Client)clients.get(i)).battleStatus) == 0) count++;
+			if (Misc.getModeFromBattleStatus(clients.get(i).battleStatus) == 0) count++;
 		if (Misc.getModeFromBattleStatus(founder.battleStatus) == 0) count++;
 		return count;
 	}
@@ -213,7 +172,7 @@ public class Battle {
 	
 	public boolean isClientInBattle(Client client) {
 		for (int i = 0; i < clients.size(); i++)
-			if (((Client)clients.get(i)) == client) return true;
+			if (clients.get(i) == client) return true;
 		if (founder == client) return true;	
 		
 		return false;
@@ -221,7 +180,7 @@ public class Battle {
 	
 	public boolean isClientInBattle(String username) {
 		for (int i = 0; i < clients.size(); i++)
-			if (((Client)clients.get(i)).account.user.equals(username)) return true;
+			if (clients.get(i).account.user.equals(username)) return true;
 		if (founder.account.user.equals(username)) return true;	
 		
 		return false;
@@ -229,17 +188,10 @@ public class Battle {
 	
 	public Client getClient(String username) {
 		for (int i = 0; i < clients.size(); i++)
-			if (((Client)clients.get(i)).account.user.equals(username)) return (Client)clients.get(i);
+			if (clients.get(i).account.user.equals(username)) return clients.get(i);
 		if (founder.account.user.equals(username)) return founder;	
 		
 		return null;
-	}
-	
-	/* returns -1 if unit is not in disabled list, index otherwise */
-	public int getUnitIndexInDisabledList(String unitname) {
-		for (int i = 0; i < disabledUnits.size(); i++)
-			if (((String)disabledUnits.get(i)).equals((unitname))) return i;
-		return -1;	
 	}
 	
 	public void sendDisabledUnitsListToClient(Client client) {
@@ -252,21 +204,46 @@ public class Battle {
 		client.sendLine("DISABLEUNITS " + line);
 	}
 	
-	public int getBot(String name) {
-		for (int i = 0; i < bots.size(); i++)
-			if (((Bot)bots.get(i)).name.equals(name)) return i;
-		return -1;	
+	/* returns number of bots in this battle (size of the bot list) */
+	public int getBotsSize() {
+		return bots.size();
 	}
-
-	/* removes first bot in bots list which is owned by client */
+	
+	/* returns Bot object of the specified bot, or null if the bot does not exist */
+	public Bot getBot(String name) {
+		for (int i = 0; i < bots.size(); i++)
+			if (bots.get(i).name.equals(name)) return bots.get(i);
+		return null;	
+	}
+	
+	/* returns null if index is out of bounds */
+	public Bot getBot(int index) {
+		try {
+			return bots.get(index);
+		} catch (IndexOutOfBoundsException e) {
+			return null;
+		}
+	}
+	
+	/* adds bot to the bot list */
+	public void addBot(Bot bot) {
+		bots.add(bot);
+	}
+	
+	/* removes first bot in the bots list which is owned by the client */
 	public boolean removeFirstBotOfClient(Client client) {
 		for (int i = 0; i < bots.size(); i++)
-			if (((Bot)bots.get(i)).ownerName.equals(client.account.user)) {
-				sendToAllClients("REMOVEBOT " + ID + " " + ((Bot)bots.get(i)).name);
+			if (bots.get(i).ownerName.equals(client.account.user)) {
+				sendToAllClients("REMOVEBOT " + ID + " " + bots.get(i).name);
 				bots.remove(i);
 				return true;
 			}
 		return false;	
+	}
+	
+	/* removes specified bot from the bot list */
+	public boolean removeBot(Bot bot) {
+		return bots.remove(bot);
 	}
 	
 	/* removes all bots owned by client */
@@ -276,7 +253,7 @@ public class Battle {
 	
 	public void sendBotListToClient(Client client) {
 		for (int i = 0; i < bots.size(); i++)
-			client.sendLine("ADDBOT " + ID + " " + ((Bot)bots.get(i)).name + " " + ((Bot)bots.get(i)).ownerName + " " + ((Bot)bots.get(i)).battleStatus + " " + ((Bot)bots.get(i)).teamColor + " " + ((Bot)bots.get(i)).AIDll);
+			client.sendLine("ADDBOT " + ID + " " + bots.get(i).name + " " + bots.get(i).ownerName + " " + bots.get(i).battleStatus + " " + bots.get(i).teamColor + " " + bots.get(i).AIDll);
 	}
 	
 	public void sendStartRectsListToClient(Client client) {
@@ -289,15 +266,20 @@ public class Battle {
 	public void sendScriptToClient(Client client) {
 		client.sendLine("SCRIPTSTART");
 		for (int i = 0; i < replayScript.size(); i++) {
-			client.sendLine("SCRIPT " + (String)replayScript.get(i));
+			client.sendLine("SCRIPT " + replayScript.get(i));
 		}
 		client.sendLine("SCRIPTEND");
 	}
 	
 	public void sendScriptToAllExceptFounder() {
 		for (int i = 0; i < clients.size(); i++) {
-			sendScriptToClient((Client)clients.get(i));
+			sendScriptToClient(clients.get(i));
 		} 
 	}
 	
+	/* will copy tempReplayScript to replayScript. This method is called
+	 * when SCRIPTEND command is received. */
+	public void ratifyTempScript() {
+		replayScript = new ArrayList<String>(tempReplayScript); 
+	}
 }

commit 1fdaefc244ffe368fcf2441e1d063b7dec92d422

fixed some serious problem with joining channels not working (introduced in r2557)
git-svn-id: http://springrts.com/svn/spring/trunk/TASServer@2559 37977431-3df6-0310-b722-df95706aa16b

Channel.java
@@ -70,7 +70,7 @@ public class Channel {
 		return !(topic.equals(""));
 	}
 	
-	/* adds new client to the list of clints of this channel */
+	/* adds new client to the list of clients of this channel */
 	public void addClient(Client client) {
 		if (isClientInThisChannel(client)) return ; // already in the channel! 
 

Channels.java
@@ -13,7 +13,11 @@ import java.util.ArrayList;
 public class Channels {
 	
 	static private ArrayList<Channel> channels = new ArrayList<Channel>();
-	
+
+	public static int getChannelsSize() {
+		return channels.size();
+	}
+
 	/* returns null if channel does not exist (is not open) */
 	public static Channel getChannel(String chanName) {
 		for (int i = 0; i < channels.size(); i++) {
@@ -30,9 +34,11 @@ public class Channels {
 			return null;
 		}
 	}	
-	
-	public static int getChannelsSize() {
-		return channels.size();
+
+	public static boolean addChannel(Channel chan) {
+		if (getChannel(chan.name) != null) return false; // channel already exists!
+		channels.add(chan);
+		return true;
 	}
 	
 	/* removes channel from channel list. Returns true if channel was found. */

Client.java
@@ -96,7 +96,7 @@ public class Client {
 			Clients.killClientDelayed(this, "Quit: channelWrite() timed out");
 			
 		} catch (Exception e) {
-			System.out.println("Error writing to socket. Line not sent! Killing the client next loop...");
+			System.out.println("Error writing to socket (exception). Line not sent! Killing the client next loop...");
 			Clients.killClientDelayed(this, "Quit: undefined connection error");
 			return false;
 		}
@@ -135,9 +135,9 @@ public class Client {
 		Channel chan = Channels.getChannel(chanName);
 		if (chan == null) {
 			chan = new Channel(chanName);
-			channels.add(chan);
+			Channels.addChannel(chan);
 		}
-		else if (this.channels.indexOf(chan) == -1) return null; // already in the channel 
+		else if (this.channels.indexOf(chan) != -1) return null; // already in the channel 
 
 		chan.addClient(this);
 		this.channels.add(chan);
@@ -163,7 +163,9 @@ public class Client {
 	 * Also notifies all clients of his departure.
 	 * Also see comments for leaveChannel() method. */
 	public void leaveAllChannels(String reason) {
-		for (int i = 0; i < this.channels.size(); i++) leaveChannel(channels.get(i), reason);
+		while (channels.size() != 0) {
+			leaveChannel(channels.get(0), reason);
+		}
 		this.channels.clear();
 	}
 	