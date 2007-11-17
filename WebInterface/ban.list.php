d82db8de5cb9d194ee141ce1430a79ec134f3eaa
betalord <betalord@37977431-3df6-0310-b722-df95706aa16b>
2007-11-17 17:54:32 +0000

    improved ban.list.php (entry deletion still not working though)
    git-svn-id: http://springrts.com/svn/spring/trunk/Lobby/TASServer@4810 37977431-3df6-0310-b722-df95706aa16b

--- WebInterface/ban.list.php
+++ WebInterface/ban.list.php
@@ -8,6 +8,11 @@
     echo "<a class='button1' href='javascript:history.go(-1)'>Go back</a>";
   }
   
+  function deleteButton($id)
+  {
+    return "<a class='button1' href='ban.delete.php?id={$id}'>Delete</a>";
+  }
+  
   function returnError($err_msg) 
   {
     printError($err_msg);
@@ -36,7 +41,74 @@
     echo '<td>'.$row{'PublicReason'}.'</td>';
     echo '</tr>';
   }
+  
+  function displayBanEntryAsTable($row)
+  {
+    if ($row{'IP_start'} == 0) 
+      $display_ip = ""; // none
+    else if ($row{'IP_start'} == $row{'IP_end'})
+      $display_ip = "IP: " . LONG2IP_($row{'IP_start'});
+    else
+      $display_ip = "IP range: " . LONG2IP_($row{'IP_start'}).' - '.LONG2IP_($row{'IP_end'});
 
+    echo '<table class="table4" width=500px>';
+    
+    echo '  <tr>';
+    echo '    <th>#'.$row{'ID'}.'</th>'; 
+    echo '    <th>'.$row{'Owner'}.'</th>'; 
+    echo '    <th>Added: '.$row{'Date'}.'</th>'; 
+    echo '    <th><div align=right>'.deleteButton($row{'ID'}).'</div></th>'; 
+    echo '  </tr>';      
+
+    echo '  <tr>';
+    echo '    <td colspan="4" class="font2">Expiration date: '.($row{'ExpirationDate'} != "" ? $row{'ExpirationDate'} : "indefinite").'</td>'; 
+    echo '  </tr>';
+
+    if ($row{'Username'} != "") {
+      echo '  <tr>';
+      echo '    <td colspan="4" class="font1"> Username: '.$row{'Username'}.'</td>';
+      echo '  </tr>';
+    }
+    
+    if ($display_ip != "") {
+      echo '  <tr>';
+      echo '    <td colspan="4" class="font1">'.$display_ip.'</td>';
+      echo '  </tr>';
+    }
+
+    if ($row{'userID'} != 0) {
+      echo '  <tr>';
+      echo '    <td colspan="4" class="font1">User ID: '.$row{'userID'}.'</td>';
+      echo '  </tr>';    
+    }
+
+    echo '  <tr>';
+    echo '    <td colspan="4">';
+    echo '      Private reason:';
+    echo '      <table class="table4" width=470px align=center>';
+    echo '        <tr><td>'.'<pre>'.$row{'PrivateReason'}.'</pre>'.'</td></tr>';
+    echo '      </table>';
+    echo '    </td>';
+    echo '  </tr>';
+
+    echo '  <tr>';
+    echo '    <td colspan="4">';
+    echo '      Public reason:';
+    echo '      <table class="table4" width=470px align=center>';
+    echo '        <tr><td>'.'<pre>'.$row{'PublicReason'}.'</pre>'.'</td></tr>';
+    echo '      </table>';
+    echo '    </td>';
+    echo '  </tr>';
+    
+    echo '</table>';
+  }  
+
+  if ($_GET['view'] != "basic") {
+    echo "<p>Click <a href='". basename($_SERVER['PHP_SELF'] . "?view=basic") . "'>here</a> for basic mode (single table).</p>";
+  } else {
+    echo "<p>Click <a href='". basename($_SERVER['PHP_SELF']) . "'>here</a> for full mode.</p>";
+  }
+  
   $select = "SELECT ID, Owner, Date, ExpirationDate, Username, IP_start, IP_end, userID, PrivateReason, PublicReason FROM BanEntries WHERE Enabled=1";
     
   // connect to the database:
@@ -50,22 +122,29 @@
   $result = mysql_query($select);
   
   // display results:
-  echo '<table class="table4" id="T1">';
-  echo '<tr>';
-  echo '<th>ID</th>'; 
-  echo '<th>Owner</th>'; 
-  echo '<th>Date added</th>'; 
-  echo '<th>Expiration date</th>'; 
-  echo '<th>Username</th>'; 
-  echo '<th>IP</th>';
-  echo '<th>userID</th>';
-  echo '<th>Private reason</th>';
-  echo '<th>Public reason</th>';
-  echo '</tr>';  
-  while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-    displayBanEntry($row);
-  } 
-  echo "</table>";
+  if ($_GET['view'] == "basic") {
+    echo '<table class="table4">';
+    echo '<tr>';
+    echo '<th>ID</th>'; 
+    echo '<th>Owner</th>'; 
+    echo '<th>Date added</th>'; 
+    echo '<th>Expiration date</th>'; 
+    echo '<th>Username</th>'; 
+    echo '<th>IP</th>';
+    echo '<th>userID</th>';
+    echo '<th>Private reason</th>';
+    echo '<th>Public reason</th>';
+    echo '</tr>';  
+    while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+      displayBanEntry($row);
+    } 
+    echo "</table>";
+  } else {
+    while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+      displayBanEntryAsTable($row);
+      echo "<br />";
+    }   
+  }
   
   mysql_close($dbh);
   
