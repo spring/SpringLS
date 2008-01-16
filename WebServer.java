2c21b91691b13494e31f1f37c4a86cb87b5e39ce
trepan <trepan@37977431-3df6-0310-b722-df95706aa16b>
2008-01-16 16:57:46 +0000

    * Removed the LuaCob script, and redirected Cob2Lua calls to LuaRules * Removed the unused CommandDesc 'hotkey' member * Added the Spring.GetUnitIsDead() call-out * Made Spring.GetPositionLosState() backwards compatible * Added 'showPanelLabel = false' to layout.lua * Removed some 0.75b2 lua compatibility code
    git-svn-id: http://springrts.com/svn/spring/trunk/Lobby/TASServer@5333 37977431-3df6-0310-b722-df95706aa16b

--- WebServer.java
+++ WebServer.java
@@ -380,29 +380,29 @@ outerloop:
     }
 
     static void fillMap() {
-        setSuffix("", "content/unknown");
-        setSuffix(".uu", "application/octet-stream");
-        setSuffix(".exe", "application/octet-stream");
-        setSuffix(".ps", "application/postscript");
-        setSuffix(".zip", "application/zip");
-        setSuffix(".sh", "application/x-shar");
-        setSuffix(".tar", "application/x-tar");
-        setSuffix(".snd", "audio/basic");
-        setSuffix(".au", "audio/basic");
-        setSuffix(".wav", "audio/x-wav");
-        setSuffix(".gif", "image/gif");
-        setSuffix(".jpg", "image/jpeg");
+        setSuffix("",      "content/unknown");
+        setSuffix(".uu",   "application/octet-stream");
+        setSuffix(".exe",  "application/octet-stream");
+        setSuffix(".ps",   "application/postscript");
+        setSuffix(".zip",  "application/zip");
+        setSuffix(".sh",   "application/x-shar");
+        setSuffix(".tar",  "application/x-tar");
+        setSuffix(".snd",  "audio/basic");
+        setSuffix(".au",   "audio/basic");
+        setSuffix(".wav",  "audio/x-wav");
+        setSuffix(".gif",  "image/gif");
+        setSuffix(".jpg",  "image/jpeg");
         setSuffix(".jpeg", "image/jpeg");
-        setSuffix(".htm", "text/html");
+        setSuffix(".htm",  "text/html");
         setSuffix(".html", "text/html");
         setSuffix(".text", "text/plain");
-        setSuffix(".c", "text/plain");
-        setSuffix(".cc", "text/plain");
-        setSuffix(".c++", "text/plain");
-        setSuffix(".h", "text/plain");
-        setSuffix(".pl", "text/plain");
-        setSuffix(".txt", "text/plain");
-        setSuffix(".log", "text/plain");
+        setSuffix(".c",    "text/plain");
+        setSuffix(".cc",   "text/plain");
+        setSuffix(".c++",  "text/plain");
+        setSuffix(".h",    "text/plain");
+        setSuffix(".pl",   "text/plain");
+        setSuffix(".txt",  "text/plain");
+        setSuffix(".log",  "text/plain");
         setSuffix(".java", "text/plain");
     }
 
