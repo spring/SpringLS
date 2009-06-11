ea32b74cf69e8fd2a18961cd3aad9a809ad22e31
satirik <satirik@37977431-3df6-0310-b722-df95706aa16b>
2009-06-11 00:09:30 +0000

    * missing file oO * TeamNo and AllyNo must both be between 0 and 200 (should be enough) * Side must be positive
    git-svn-id: http://springrts.com/svn/spring/Lobby/TASServer@7268 37977431-3df6-0310-b722-df95706aa16b

---
+++ BattleStatus.java
@@ -0,0 +1,65 @@
+import sun.org.mozilla.javascript.internal.UintMap;
+
+import com.sun.xml.internal.bind.v2.model.core.ID;
+
+
+public class BattleStatus {
+	public int TeamNo;
+	public int AllyNo;
+	public Boolean Ready;
+	public int Mode;
+	public int Handicap;
+	public int Side;
+	
+	public Boolean Update(String s)
+	{
+		int oldTeamNo = TeamNo;
+		int oldAllyNo = AllyNo;
+		Boolean oldReady = Ready;
+		int oldMode = Mode;
+		int oldHandicap = Handicap;
+		int oldSide = Side;
+		
+		String[] items = s.split("\t");
+		
+		try
+		{
+			TeamNo = Integer.valueOf(items[0]);
+			assert(TeamNo>=0);
+			assert(TeamNo<=200);
+			AllyNo = Integer.valueOf(items[1]);
+			assert(AllyNo>=0);
+			assert(AllyNo<=200);
+			Ready = Integer.valueOf(items[2]) != 0;
+			Mode = Integer.valueOf(items[3]);
+			Handicap = Integer.valueOf(items[4]);
+			Side = Integer.valueOf(items[5]);
+			assert(Side>=0);
+			return true;
+		}catch(Exception e)
+		{
+			TeamNo = oldTeamNo;
+			AllyNo = oldAllyNo;
+			Ready = oldReady;
+			Mode = oldMode;
+			Handicap = oldHandicap;
+			Side = oldSide;
+			return false;
+		}
+	}
+	
+	public Boolean isSpectator()
+	{
+		return Mode == 0;
+	}
+	
+	public String toString()
+	{
+		int rdy;
+		if(Ready)
+			rdy = 1;
+		else
+			rdy = 0;
+		return TeamNo+"\t"+AllyNo+"\t"+rdy+"\t"+Mode+"\t"+Handicap+"\t"+Side;
+	}
+}
