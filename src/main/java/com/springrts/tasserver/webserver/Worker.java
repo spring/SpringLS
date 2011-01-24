4914fe376264429ffb9af067568f505f7805656f
hoijui <hoijui.quaero@gmail.com>
2011-01-25 00:30:00 +0100

    adjust email address in copyright headers [minor]

--- src/main/java/com/springrts/tasserver/webserver/Worker.java
+++ src/main/java/com/springrts/tasserver/webserver/Worker.java
@@ -1,5 +1,5 @@
 /*
-	Copyright (c) 2010 Robin Vobruba <robin.vobruba@derisk.ch>
+	Copyright (c) 2010 Robin Vobruba <hoijui.quaero@gmail.com>
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
