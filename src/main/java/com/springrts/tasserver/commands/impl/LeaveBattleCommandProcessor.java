5f248c6603bd1abd1e27b874f2279fd4fc2bc870
hoijui <hoijui.quaero@gmail.com>
2011-01-24 20:55:14 +0100

    fix copyright year [minor]

--- src/main/java/com/springrts/tasserver/commands/impl/LeaveBattleCommandProcessor.java
+++ src/main/java/com/springrts/tasserver/commands/impl/LeaveBattleCommandProcessor.java
@@ -1,5 +1,5 @@
 /*
-	Copyright (c) 2010 Robin Vobruba <robin.vobruba@derisk.ch>
+	Copyright (c) 2011 Robin Vobruba <robin.vobruba@derisk.ch>
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
